f3d770194d27a677545814779f4a33b5370c2e87
Byte <32001480+RenderingByte@users.noreply.github.com>
2022-06-26 17:26:42 -0400

    Delete Parse All.bat

--- Parse All.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title RhythmGame Parser
-echo All your osu! mania maps are being parsed.
-echo Please be patient as this process can take time depending on how many maps you have.
-echo Currently, maps that have already been parsed will not be reparsed.
-echo ------------------------------------------------------------------------------------
-node parser.js
-pause
